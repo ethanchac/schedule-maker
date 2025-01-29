2105fec29b89964ccc4711b6743e0f24a303a8e2
ethan <ethancha1011@gmail.com>
2025-01-28 19:49:15 -0500

    created the variable where you can find how many hours the store opens for

--- script.js
+++ script.js
@@ -1,24 +1,37 @@
+let hoursInSchedule = 0;
+
 function populateTimeOptions() {
     const startTimeSelect = document.getElementById('startTime');
     const endTimeSelect = document.getElementById('endTime');
 
-    for (let hour = 6; hour <= 21; hour++) {
+    for (let hour = 1; hour <= 12; hour++) {
         let displayHour = hour > 12 ? `${hour - 12} PM` : `${hour} AM`;
-        startTimeSelect.options.add(new Option(displayHour, hour));
-        endTimeSelect.options.add(new Option(displayHour, hour));
+        startTimeSelect.options.add(new Option(hour + " AM"));
+        endTimeSelect.options.add(new Option(hour + " PM"));
     }
+    
 }
 
-function toggleTimeSelector() {
-    const timeSelector = document.getElementById('timeSelector');
-    timeSelector.style.display = timeSelector.style.display === 'none' ? 'block' : 'none';
-}
 
 function applyHours() {
     const startTime = document.getElementById('startTime').value;
     const endTime = document.getElementById('endTime').value;
-    console.log(`Working hours set from ${startTime} to ${endTime}`);
-    toggleTimeSelector(); // Hide selector after applying hours
+    
+    //To calculate the amount of hours in the day to work
+    var startingHours;
+    var endingHours;
+    if(startTime.length == 5){
+        startingHours = Number(startTime.substring(0, 2));
+    }else{
+        startingHours = Number(startTime.substring(0,1));
+    }
+    if(endTime.length == 5){
+        endingHours = Number(endTime.substring(0,2));
+    }else{
+        endingHours = Number(endTime.substring(0,1));
+    }
+    hoursInSchedule = endingHours + 12 - startingHours;
+
 }
 
 window.onload = function() {
